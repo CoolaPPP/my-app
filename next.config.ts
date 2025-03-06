d929cd775bf5e1d492d362322c3fab60f59b524d
CoolaPPP <coola2.2544@gmail.com>
2025-03-06 21:32:44 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337", 
+        protocol: "https",
+        hostname: "server-fl4l.onrender.com",
+        port: "", 
         pathname: "/uploads/**/*",
       },
     ],
