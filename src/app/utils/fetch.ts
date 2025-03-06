295c63d796cb92475a3bb58db57db2125c6a9a28
CoolaPPP <coola2.2544@gmail.com>
2025-03-06 15:07:24 +0700

    init

---
+++ src/app/utils/fetch.ts
@@ -0,0 +1,62 @@
+import { cookies } from "next/headers";
+import qs from "qs";
+
+interface FetchResponse<T> {
+    data: T | null;
+    status?: number; 
+    error?: {};
+  }
+   export const fetchApi = async <T>(
+    path: string,
+    options: RequestInit & {} ={
+      method: "GET",
+    },
+    populate?: any,
+    filter?: any
+  ): Promise<FetchResponse<T>> => {
+    let headers = {};
+  const coockie = await cookies();
+  const accessToken = coockie.get("access_token")?.value || "";
+  if (accessToken !== "") {
+    headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    };
+  } else {
+    headers = {
+      "Content-Type": "application/json",
+    };
+  }
+    // let url = `${process.env.API_URL}${path}`
+    let url: any;
+    if (populate) {
+        let queryParams: any = {};
+        queryParams.populate = populate;
+        if (filter) {
+            queryParams.filters = filter;
+        }
+        const newUrl = new URL(path, process.env.API_URL);
+        // newUrl.search = qs.stringify({
+        //     populate: queryParams.populate
+        // })
+        newUrl.search = qs.stringify(queryParams);    
+        url = newUrl;
+    }
+    try {
+        const response = await fetch(url, { ...options, headers });
+        if (!response.ok) {
+            throw new Error("Failed to fetch team members");
+        }
+
+        const result = await response.json();
+        return { 
+            data: result,
+        status: response.status,
+            
+        }
+    }catch (error: unknown) {
+        return{data: null, status: 500, error: "error"}
+     }
+    }
+
+     
